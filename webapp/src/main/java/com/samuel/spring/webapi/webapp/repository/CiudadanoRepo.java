a862ce7cc1414efd4c74cca8ecdf588d5ce46e67
SamAntunez <84483432+SamAntunez@users.noreply.github.com>
2024-04-20 16:26:47 -0400

    Delete webapp directory

--- webapp/src/main/java/com/samuel/spring/webapi/webapp/repository/CiudadanoRepo.java
+++
@@ -1,7 +0,0 @@
-package com.samuel.spring.webapi.webapp.repository;
-
-import com.samuel.spring.webapi.webapp.model.Ciudadano;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CiudadanoRepo extends JpaRepository<Ciudadano, Long> {
-}
--- webapp/src/main/java/com/samuel/spring/webapi/webapp/repository/TareaRepo.java
+++
@@ -1,7 +0,0 @@
-package com.samuel.spring.webapi.webapp.repository;
-
-import com.samuel.spring.webapi.webapp.model.Tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TareaRepo extends JpaRepository<Tarea, Long> {
-}
